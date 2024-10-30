556ce3fb7f29dce1a425e084abc8d4c0f13c3484
TeamFun-best-server <minecraftgame.jonathan@gmail.com>
2024-10-30 11:32:25 +0100

    Update BedClutchMaps.java

--- src/me/hi12167pies/BedClutch/Commands/BedClutchMaps.java
+++ src/me/hi12167pies/BedClutch/Commands/BedClutchMaps.java
@@ -1,12 +1,18 @@
 package me.hi12167pies.BedClutch.Commands;
 
 import me.hi12167pies.BedClutch.Config;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+import java.util.Random;
+
 public class BedClutchMaps implements CommandExecutor {
+    private Random random = new Random();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (!(sender instanceof Player)) return false;
@@ -17,19 +23,31 @@ public class BedClutchMaps implements CommandExecutor {
         Player player = (Player) sender;
 
         if (args.length >= 2) {
+            String map = args[1];
             if (args[0].equalsIgnoreCase("setspawn")) {
-                String map = args[1];
                 Config.data.saveMapSpawn(map, player.getLocation());
                 player.sendMessage("§aSet the map's spawn to your current location");
             } else if (args[0].equalsIgnoreCase("delete")) {
-                String map = args[1];
-                if (!Config.data.mapExist(map))
+                if (!Config.data.mapExist(map)) {
                     player.sendMessage("§cThat map doesn't exist");
-                else
+                } else {
                     Config.data.deleteMap(map);
-            } else sender.sendMessage(command.getUsage());
-        } else sender.sendMessage(command.getUsage());
+                }
+            } else {
+                player.sendMessage(command.getUsage());
+            }
+        } else {
+            player.sendMessage(command.getUsage());
+        }
 
         return true;
+    }
+
+    public void teleportToRandomSpawn(Player player, String map) {
+        List<Location> spawns = Config.data.getMapSpawns(map);
+        if (spawns != null && !spawns.isEmpty()) {
+            Location randomSpawn = spawns.get(random.nextInt(spawns.size()));
+            player.teleport(randomSpawn);
+        }
     }
 }
